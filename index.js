95a750c5627513e087df5a62e72205da607291f8
rahul <rahulmisra2000@gmail.com>
2020-12-25 19:47:47 -0500

    My GAPI Samples

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ var http = require('http');
 var https = require('https');
 var express = require('express');
 
+process.on('uncaughtException', err => console.error('uncaughtException', err))
+process.on('unhandledRejection', err => console.error('unhandledRejection', err))
 
 var app = express();
 app.use(express.static('public'));
 var httpServer = http.createServer(app);
 
-
 // This was created using openssl (under Git for Windows installation) using instructions in the first part of 
 // https://nodejs.org/en/knowledge/HTTP/servers/how-to-create-a-HTTPS-server/
+
 var privateKey  = fs.readFileSync('..\\..\\certs\\key.pem', 'utf8');
 var certificate = fs.readFileSync('..\\..\\certs\\cert.pem', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
@@ -18,7 +20,11 @@ var credentials = {key: privateKey, cert: certificate};
 var httpsServer = https.createServer(credentials, app);
 
 
-httpServer.listen(8080);
-httpsServer.listen(8443);
+httpServer.listen(8080,() => {
+    console.log('Server started at 8080');
+   });
+httpsServer.listen(8443, () => {
+    console.log('Server started at 8443');
+   });
 
 console.log("here");
